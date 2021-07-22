95f5d89dc07d24cf44c0c43b5968568f4e8ce9d6
David Eleuterius <davideleuterius@gmail.com>
2021-07-22 12:48:40 -0500

    creating EMaaS branch

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,1 +1,21 @@
-console.log( '- initialized -' );
+const iconLabels = document.querySelectorAll('label.iconLabel');
+for (let i = 0; i < iconLabels.length; i++) {
+  iconLabels[i].addEventListener('click', function () {
+    if (!this.classList.contains('iconCheckbox')) {
+      for (let sibling of this.parentNode.parentNode.children) {
+        if (sibling !== this) sibling.classList.remove('selected');
+      }
+    }
+    this.parentNode.classList.toggle('selected');
+  });
+}
+
+const radioLabels = document.querySelectorAll('.radioGroup label');
+for (let i = 0; i < radioLabels.length; i++) {
+  radioLabels[i].addEventListener('click', function() {
+    for (let sibling of this.parentNode.parentNode.children) {
+      if (sibling !== this) sibling.classList.remove('selected');
+    }
+    this.parentNode.classList.toggle('selected');
+  });
+}
